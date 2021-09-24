e9ffad241bc047efe5f5db50d9f41ab5a1600659
b0und <f1arenova1@gmail.com>
2021-09-24 22:05:56 +0300

    ripple wip

--- js/image-switch.js
+++ js/image-switch.js
@@ -7,21 +7,39 @@ const artSection = document.getElementsByClassName('art-section')
 
 let isPressed = false
 
+const waveAnimation = anime({
+  targets: '.art-wave',
+  r: 200,
+  opacity: [
+    { value: 0.8, duration: 250 },
+    { value: 0.0, duration: 1250 },
+  ],
+  'stroke-width': [5, 18, 0],
+  duration: 2000,
+  delay: anime.stagger(500),
+  easing: 'easeInOutSine',
+  loop: true,
+})
+
+waveAnimation.play()
+
 artBtn.addEventListener('click', () => {
   isPressed = !isPressed
   artBtn.classList.toggle('thumbnail-text-style')
 
   if (isPressed) {
     artBtn.textContent = 'thumbnails'
-
     img1.src = 'assets/meme-img1.jpg'
     img2.src = 'assets/meme-img2.jpg'
     img3.src = 'assets/meme-img3.jpg'
+    waveAnimation.restart()
+    waveAnimation.pause()
   } else {
     artBtn.textContent = 'art'
     img1.src = 'assets/art-img3.jpg'
     img2.src = 'assets/art-img2.jpg'
     img3.src = 'assets/art-img1.jpg'
+    waveAnimation.play()
   }
   document
     .getElementById('art-section')
--- js/wah.js
+++ js/wah.js
@@ -1,13 +1,39 @@
-// anime({
-//   targets: '.wah-button',
-//   filter: "brightness(1)",
-//   duration: 250,
-// })
+// DEBUG
+var gui = new dat.GUI()
+var wahFolder = gui.addFolder('Wah section')
+wahFolder.open()
 
 var targetElm = document.querySelector('.wah-button img')
 var audio = new Audio('assets/ina-wah-echo.mp3')
 
-var tl = anime.timeline({
+var props = {
+  staggerDelay: 500,
+}
+
+const waveOutAnimation = (props) =>
+  anime({
+    targets: '.wah-wave',
+    r: 200,
+    opacity: [
+      { value: 0.8, duration: 250 },
+      { value: 0.0, duration: 1250 },
+    ],
+    'stroke-width': [5, 26, 0],
+    duration: 2000,
+    delay: anime.stagger(props.staggerDelay),
+    easing: 'easeInOutSine',
+    loop: true,
+  })
+
+waveOutAnimation(props).play()
+
+wahFolder.add(props, 'staggerDelay', 0, 2000).onChange((newVal) => {
+  props.staggerDelay = newVal
+  waveOutAnimation().restart()
+  waveOutAnimation(props).play()
+})
+
+var brightnessTimeline = anime.timeline({
   autoplay: false,
   duration: 2000,
   complete: function () {
@@ -15,7 +41,7 @@ var tl = anime.timeline({
   },
 })
 
-tl.add({
+brightnessTimeline.add({
   duration: 2000,
   easing: 'easeInOutExpo',
   update: function (anim) {
@@ -24,7 +50,7 @@ tl.add({
 })
 
 const wahClick = () => {
-  tl.play()
+  brightnessTimeline.play()
   setTimeout(() => {
     audio.play()
   }, 1000)
