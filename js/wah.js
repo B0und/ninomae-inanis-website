9cd5ced2acf0776ae525fbb4bc44fb0edd479b30
b0und <f1arenova1@gmail.com>
2021-09-29 14:51:28 +0300

    wave-out anim tweaks

--- js/wah.js
+++ js/wah.js
@@ -1,59 +1,65 @@
-// DEBUG
-var gui = new dat.GUI()
-var wahFolder = gui.addFolder('Wah section')
-wahFolder.open()
-
 var targetElm = document.querySelector('.wah-button img')
 var audio = new Audio('assets/ina-wah-echo.mp3')
 
-var props = {
+let props = {
   staggerDelay: 500,
+  radius: 200,
 }
 
-const waveOutAnimation = (props) =>
-  anime({
-    targets: '.wah-wave',
-    r: 200,
-    opacity: [
-      { value: 0.8, duration: 250 },
-      { value: 0.0, duration: 1250 },
-    ],
-    'stroke-width': [5, 26, 0],
+const waveOutAnimation = anime({
+  targets: '.wah-wave-1',
+  r: [20, 70],
+  opacity: [
+    { value: 0.5, duration: 800 },
+    { value: 0.0, duration: 600 },
+  ],
+  'stroke-width': [10, 20, 0],
+  duration: 3000,
+  easing: 'easeInOutSine',
+  loop: true,
+})
+
+// const waveInAnimation = anime({
+//   targets: '.wah-wave',
+//   r: [100, 10],
+//   opacity: [
+//     { value: 0.5, duration: 250 },
+//     { value: 0.0, duration: 1000 },
+//   ],
+//   'stroke-width': [10, 20, 0],
+//   duration: 2500,
+//   easing: 'easeInOutSine',
+//   loop: true,
+// })
+
+var wahTimeline = anime.timeline({
+  autoplay: false,
+  duration: 3000,
+  complete: function () {
+    targetElm.style.filter =
+      'brightness(0.05) drop-shadow(0px 0px 20px rgba(177, 91, 226, 0.733)'
+    waveOutAnimation.play()
+  },
+})
+
+wahTimeline
+  .add({
     duration: 2000,
-    delay: anime.stagger(props.staggerDelay),
-    easing: 'easeInOutSine',
-    loop: true,
+    easing: 'easeInOutExpo',
+    update: function (anim) {
+      targetElm.style.filter = 'brightness(' + anim.progress / 100 + ')'
+    },
+  })
+  .add({
+    begin: () => {
+      audio.play()
+    },
   })
 
-waveOutAnimation(props).play()
-
-wahFolder.add(props, 'staggerDelay', 0, 2000).onChange((newVal) => {
-  props.staggerDelay = newVal
-  waveOutAnimation().restart()
-  waveOutAnimation(props).play()
-})
-
-var brightnessTimeline = anime.timeline({
-  autoplay: false,
-  duration: 2000,
-  complete: function () {
-    targetElm.style.filter = 'brightness(0.05)'
-  },
-})
-
-brightnessTimeline.add({
-  duration: 2000,
-  easing: 'easeInOutExpo',
-  update: function (anim) {
-    targetElm.style.filter = 'brightness(' + anim.progress / 100 + ')'
-  },
-})
-
 const wahClick = () => {
-  brightnessTimeline.play()
-  setTimeout(() => {
-    audio.play()
-  }, 1000)
+  waveOutAnimation.restart()
+  waveOutAnimation.pause()
+  wahTimeline.play()
 }
 
 document.querySelector('.wah-button img').onclick = wahClick
